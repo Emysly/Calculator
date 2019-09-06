f818552ed8dc69cbcf84ef0ad3e029b83bbe680a
Emysilva <emysly12@gmail.com>
2019-09-06 23:48:28 +0100

    added the functions for the operators

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,48 +1,27 @@
-let input = document.querySelector('.calc-display');
+document.querySelector(".clear").addEventListener("click", () => {
+  document.querySelector(".calc-display").value = "";
+  document.querySelector(".calc-result").value = "";
+});
 
-document.querySelector('.keypad').addEventListener('click', e => {
-  e.preventDefault();
-
-  if(e.target.classList.contains('container')) {
-    return;
-  } else {
-  input.value += e.target.value;
-  }
-  
-const inputArray = input.value.split(',');
-console.log(inputArray);
-
-let a = inputArray[0];
-let b = inputArray[1];
-
-console.log(inputArray[0]);
-
-let operator;
-if (e.target.classList.contains('btn-operator')) {
-operator = document.querySelector('.btn-operator').value;
-console.log(operator);
-}
-switch(operator) {
-  case '+':
-  input.value = '';
-   inputArray.reduce((a, b) => parseFloat(a) + parseFloat(b));
-    break;
-  case '-':
-      inputArray.reduce((a, b) => parseFloat(a) - parseFloat(b));
-      break;
-  case '*':
-      inputArray.reduce((a, b) => parseFloat(a) * parseFloat(b));
-      break;
-  case '/':
-      inputArray.reduce((a, b) => parseFloat(a) / parseFloat(b));
-      break;
-  default :
-      return '';
+function input(value) {
+  document.querySelector(".calc-display").value += value;
 }
 
-})
+function equal() {
+  const exp = document.querySelector(".calc-display").value;
 
-document.querySelector('.btn-orange').addEventListener('click', e => {
-  
-})
+  document.querySelector(".calc-result").value = eval(exp);
+}
+function back() {
+  const exp = document.querySelector(".calc-display").value;
 
+  document.querySelector(".calc-display").value = exp.slice(0, exp.length - 1);
+}
+
+function dot() {
+  document.querySelector(".calc-display").value = "0.";
+}
+
+function on() {
+  document.querySelector(".calc-display").value = "0";
+}
